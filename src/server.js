f6d9e096ef12cc01e51216f8f4439c2750e315ad
ZhaAbhi <jhabhishek.developer@gmail.com>
2023-10-16 20:45:22 +0545

    Installed jest modules

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const app = require("./app");
 const PORT = 8000;
 const server = http.createServer(app);
 
+//starts the server
 function startServer() {
   server.listen(PORT, () => {
     console.log(`Server Running on PORT ${PORT}`);
